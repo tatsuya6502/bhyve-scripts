2605a85811c2742b93eae786c7dd7177927a13b9
Tatsuya Kawano <tatsuya@hibaridb.org>
2015-09-11 21:49:06 +0800

    Add centos6-vertica

--- cleanup.sh
+++ cleanup.sh
@@ -5,12 +5,16 @@ ifconfig tap0 down
 ifconfig tap1 down
 ifconfig tap2 down
 ifconfig tap3 down
+ifconfig tap4 down
+ifconfig tap5 down
 
 ifconfig bridge0 destroy
 ifconfig tap0 destroy
 ifconfig tap1 destroy
 ifconfig tap2 destroy
 ifconfig tap3 destroy
+ifconfig tap4 destroy
+ifconfig tap5 destroy
 
 kldunload vmm
 kldunload nmdm
--- setup.sh
+++ setup.sh
@@ -7,14 +7,25 @@ kldload nmdm
 sysctl net.link.tap.up_on_open=1
 sysctl net.inet.ip.forwarding=1
 
+# tap0: centos7-docker1
+# tap1: ubuntu-14.04
+# tap2: rhel7-ost-juno
+# tap3: rhel7-ost-icehouse
+# tap4: (unused)
+# tap5: centos6-vertica
+
 ifconfig tap0 create
 ifconfig tap1 create
 ifconfig tap2 create
 ifconfig tap3 create
+ifconfig tap4 create
+ifconfig tap5 create
 
 ifconfig bridge0 create
 ifconfig bridge0 addm tap0 addm bge0 up
+# ifconfig bridge0 addm tap0 addm wlan0 up
 ifconfig bridge0 addm tap1
 ifconfig bridge0 addm tap2
 ifconfig bridge0 addm tap3
-
+ifconfig bridge0 addm tap4
+ifconfig bridge0 addm tap5
