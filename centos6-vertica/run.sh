5605da0715d238a5dc0e94c718327c03b5b2650a
Tatsuya Kawano <tatsuya@hibaridb.org>
2016-07-27 13:05:30 +0900

    Fix the tap device name in a run script for centos6-vertica

--- centos6-vertica/run.sh
+++ centos6-vertica/run.sh
@@ -10,7 +10,7 @@ grub-bhyve -m device.map -r hd0,msdos1 -M 4096M centos6-vertica < ./grub.in > /d
 bhyve -c 4 -m 4096M -H -P -A -l com1,stdio \
       -s 0:0,hostbridge \
       -s 1:0,lpc \
-      -s 2:0,virtio-net,tap4 \
+      -s 2:0,virtio-net,tap5 \
       -s 3,virtio-blk,/dev/zvol/zroot0/bhyve/centos6-vertica/disk1 \
       centos6-vertica
 
