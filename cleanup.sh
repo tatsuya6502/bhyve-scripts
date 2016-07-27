e4fa3dc57522ae519657a529a4f0d7ba4de8c49c
Tatsuya Kawano <tatsuya@hibaridb.org>
2016-07-27 13:06:48 +0900

    Add a network configuration via wlan0 (WiFi)

--- cleanup.sh
+++ cleanup.sh
@@ -7,6 +7,8 @@ ifconfig tap2 down
 ifconfig tap3 down
 ifconfig tap4 down
 ifconfig tap5 down
+ifconfig tap6 down
+ifconfig tap7 down
 
 ifconfig bridge0 destroy
 ifconfig tap0 destroy
@@ -15,7 +17,8 @@ ifconfig tap2 destroy
 ifconfig tap3 destroy
 ifconfig tap4 destroy
 ifconfig tap5 destroy
+ifconfig tap6 destroy
+ifconfig tap7 destroy
 
 kldunload vmm
 kldunload nmdm
-
--- setup.sh
+++ setup.sh
@@ -13,6 +13,8 @@ sysctl net.inet.ip.forwarding=1
 # tap3: rhel7-ost-icehouse
 # tap4: openbsd
 # tap5: centos6-vertica
+# tap6: rhel7-ost-liberty (external)
+# tap7: rhel7-ost-liberty (internal)
 
 ifconfig tap0 create
 ifconfig tap1 create
@@ -20,12 +22,16 @@ ifconfig tap2 create
 ifconfig tap3 create
 ifconfig tap4 create
 ifconfig tap5 create
+ifconfig tap6 create
+ifconfig tap7 create
 
 ifconfig bridge0 create
-ifconfig bridge0 addm tap0 addm bge0 up
-# ifconfig bridge0 addm tap0 addm wlan0 up
+ifconfig bridge0 addm bge0 up
+ifconfig bridge0 addm tap0
 ifconfig bridge0 addm tap1
 ifconfig bridge0 addm tap2
 ifconfig bridge0 addm tap3
 ifconfig bridge0 addm tap4
 ifconfig bridge0 addm tap5
+ifconfig bridge0 addm tap6
+ifconfig bridge0 addm tap7
