3862cdd1ee81bec591960dd634b1755e12e9ef3f
Tatsuya Kawano <tatsuya@hibaridb.org>
2017-02-22 12:37:04 +0800

    Change the name of the zpool on Mac mini.

--- centos6-hibari3/run.sh
+++ centos6-hibari3/run.sh
@@ -11,7 +11,7 @@ bhyve -c 4 -m 4096M -H -P -A -l com1,stdio \
       -s 0:0,hostbridge \
       -s 1:0,lpc \
       -s 2:0,virtio-net,tap0 \
-      -s 3,virtio-blk,/dev/zvol/zroot0/bhyve/centos6-hibari3/disk1 \
+      -s 3,virtio-blk,/dev/zvol/zroot/bhyve/centos6-hibari3/disk1 \
       centos6-hibari3
 
 bhyvectl --destroy --vm=centos6-hibari3
--- openbsd-dev/run.sh
+++ openbsd-dev/run.sh
@@ -11,7 +11,7 @@ bhyve -W -c 4 -m 4096M -H -P -A -l com1,stdio \
       -s 0:0,hostbridge \
       -s 1:0,lpc \
       -s 2:0,virtio-net,tap4 \
-      -s 3,virtio-blk,/dev/zvol/zroot0/bhyve/openbsd-dev/disk1 \
+      -s 3,virtio-blk,/dev/zvol/zroot/bhyve/openbsd-dev/disk1 \
       openbsd-dev
 
 bhyvectl --destroy --vm=openbsd-dev
