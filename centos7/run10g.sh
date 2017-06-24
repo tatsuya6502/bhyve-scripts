de4c79fa7a8fc68220d98c35e9cc08d9719ab7e5
Tatsuya Kawano <tatsuya6502@gmail.com>
2017-06-24 12:11:19 +0800

    Add 10GB RAM configuration for CentOS 7

---
+++ centos7/run10g.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+bhyvectl --destroy --vm=centos7-docker1
+rm *.core
+
+set e
+
+grub-bhyve -m device.map -r hd0,msdos1 -M 10240M centos7-docker1 < ./grub.in > /dev/null
+
+bhyve -c 4 -m 10240M -H -P -A -l com1,stdio \
+      -s 0:0,hostbridge \
+      -s 1:0,lpc \
+      -s 2:0,virtio-net,tap0 \
+      -s 3,virtio-blk,/dev/zvol/zroot/bhyve/centos7-docker/disk1 \
+      centos7-docker1
+
+bhyvectl --destroy --vm=centos7-docker1
