40c528cb6ad2f503f5921fa40cd27921cd9e681e
Tatsuya Kawano <tatsuya6502@gmail.com>
2017-06-24 12:09:16 +0800

    Update RHEL 7 OST Mitaka
    - Set the system clock to UTC.
    - Add disk2 to store RPM local repos.

--- rhel7-ost-mitaka/run.sh
+++ rhel7-ost-mitaka/run.sh
@@ -4,6 +4,9 @@ VM_NAME=rhel7-ost-mitaka
 RAM_SIZE=4096M
 TAP_DEV=tap7
 
+DISK1=/dev/zvol/zroot/bhyve/rh-ost-mitaka/disk1
+DISK2=/dev/zvol/zroot/bhyve/rh-ost-mitaka/disk2
+
 bhyvectl --destroy --vm=$VM_NAME
 rm *.core
 
@@ -11,24 +14,26 @@ set e
 
 # Booting from live DVD, with fbuf
 # ---------------------------------------------
-# bhyve -c 4 -m $RAM_SIZE -H -P -A \
+# bhyve -c 4 -m $RAM_SIZE -H -P -A -u \
 #       -s 0:0,hostbridge \
 #       -s 1:0,lpc \
 #       -s 2:0,virtio-net,$TAP_DEV \
 #       -s 3,ahci-cd,/home/tatsuya/installers/rhel-7.3/rhel-server-7.3-x86_64-dvd.iso \
-#       -s 4,virtio-blk,/dev/zvol/zroot/bhyve/rh-ost-mitaka/disk1 \
+#       -s 4,virtio-blk,$DISK1 \
+#       -s 5,virtio-blk,$DISK2 \
 #       -s 29,fbuf,tcp=0.0.0.0:5900,w=1920,h=1080 \
 #       -s 30,xhci,tablet \
 #       -l bootrom,/usr/local/share/uefi-firmware/BHYVE_UEFI.fd \
 #       $VM_NAME
 
-# Booting from hard drive
+# Booting from hard drive, with fbuf
 # ---------------------------------------------
-bhyve -c 4 -m $RAM_SIZE -H -P -A \
+bhyve -c 4 -m $RAM_SIZE -H -P -A -u \
       -s 0:0,hostbridge \
       -s 1:0,lpc \
       -s 2:0,virtio-net,$TAP_DEV \
-      -s 3,virtio-blk,/dev/zvol/zroot/bhyve/rh-ost-mitaka/disk1 \
+      -s 3,virtio-blk,$DISK1 \
+      -s 4,virtio-blk,$DISK2 \
       -s 29,fbuf,tcp=0.0.0.0:5900,w=1280,h=800 \
       -s 30,xhci,tablet \
       -l bootrom,/usr/local/share/uefi-firmware/BHYVE_UEFI.fd \
