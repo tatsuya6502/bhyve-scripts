bb2544aaef6e44fd5ba67903af4599a8a3486e2e
Tatsuya Kawano <tatsuya@hibaridb.org>
2018-08-03 21:58:39 +0800

    Rename fedora25 to fedora

--- fedora/run.sh
+++ fedora/run.sh
@@ -1,19 +1,24 @@
 #!/bin/sh
 
-VM_NAME=fedora25
+VM_NAME=fedora
+NUM_CPUS=2
 RAM_SIZE=4096M
+# RAM_SIZE=8192M
 TAP_DEV=tap3
+DISK1=/dev/zvol/zdata77/bhyve/fedora/disk1
+DISK2=/dev/zvol/zdata77/bhyve/fedora/disk2
 
 bhyvectl --destroy --vm=$VM_NAME
 rm *.core
 
-set e
+set -e
 
-bhyve -c 4 -m $RAM_SIZE -H -P -A \
+bhyve -c $NUM_CPUS -m $RAM_SIZE -H -P -A \
       -s 0:0,hostbridge \
       -s 1:0,lpc \
       -s 2:0,virtio-net,$TAP_DEV \
-      -s 3,virtio-blk,/dev/zvol/zroot/bhyve/fedora25/disk1 \
+      -s 3,virtio-blk,$DISK1 \
+      -s 4,virtio-blk,$DISK2 \
       -s 29,fbuf,tcp=0.0.0.0:5900,w=1920,h=1080 \
       -s 30,xhci,tablet \
       -l bootrom,/usr/local/share/uefi-firmware/BHYVE_UEFI.fd \
